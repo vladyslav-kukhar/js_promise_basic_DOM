fc7b6f717a72bdd3f174f4001fe2db8a0491ff49
Vladyslav <vladislavkukhar66@gmail.com>
2026-07-13 21:59:49 +0300

    add than and catch to all promises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,11 @@
 'use strict';
 
 // write your code here
-const promise1 = new Promise((resolve) => {
+const body = document.querySelector('body');
+
+const promise1 = new Promise((resolve, reject) => {
   document.querySelector('.logo').addEventListener('click', resolve);
 });
-
-const body = document.querySelector('body');
 
 promise1.then((resolve) => {
   const div = document.createElement('div');
@@ -16,8 +16,27 @@ promise1.then((resolve) => {
   body.append(div);
 });
 
+promise1.catch((reject) => {
+  const div = document.createElement('div');
+
+  div.classList.add('message');
+  div.classList.add('error-message');
+  div.textContent = 'Promise was rejected!';
+
+  body.append(div);
+});
+
 const promise2 = new Promise((resolve, reject) => {
   setTimeout(reject, 3000);
+});
+
+promise2.then((resolve) => {
+  const div = document.createElement('div');
+
+  div.classList.add('message');
+  div.textContent = 'Promise was resolved!';
+
+  body.append(div);
 });
 
 promise2.catch((reject) => {
